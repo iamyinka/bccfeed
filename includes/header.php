d3dd0f111777ee8ca1dcfc1206ae4a7556b91859
Yinka Ash <developer@designsbynetguru.com>
2017-09-05 04:11:57 +0800

    Create Navigation Menu

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,8 @@ require 'config/config.php';
 if (isset($_SESSION['username'])) {
 	# code...
 	$userLoggedIn = $_SESSION['username'];
+	$user_details_query = mysqli_query($connection, "SELECT * FROM users WHERE username='$userLoggedIn'");
+	$user = mysqli_fetch_array($user_details_query);
 } else {
 	header("Location: register.php");
 }
@@ -17,5 +19,34 @@ if (isset($_SESSION['username'])) {
 <head>
 	<meta charset="UTF-8">
 	<title>BCC FEED</title>
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="assets/js/jquery-3.2.1.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
 </head>
 <body>
+
+<div class="top_bar navbar-fixed-top">
+	<div class="logo pull-left">
+		<a href="index.php">BCC Social</a>
+	</div>
+
+	<nav class="pull-right">
+		<a href="<?php echo $userLoggedIn; ?>">
+			<?php  
+
+				echo $user['first_name'];
+
+			?> <i class="fa fa-user-plus fa-lg"></i>
+		</a>
+		<a href="index.php"> <i class="fa fa-home fa-lg"></i></a>
+		<a href="#"> <i class="fa fa-envelope fa-lg"></i></a>
+		<a href="#"> <i class="fa fa-bell-o fa-lg"></i></a>
+		<a href="#"> <i class="fa fa-users fa-lg"></i></a>
+		<a href="#"> <i class="fa fa-cog fa-lg"></i></a>
+		<a href="#"> <i class="fa fa-sign-out fa-lg"></i></a>
+	</nav>
+</div>
+
+<div class="container">
--- index.php
+++ index.php
@@ -1,11 +1,58 @@
 <?php 
 
 include 'includes/header.php';
-session_destroy();
+// session_destroy();
 
 ?>
 
-<h1>Welcome to Index Page</h1>
+
+	<div class="row">
+		<div class="col-sm-4">
+			<div class="user_details clearfix">
+				<a href="<?php echo $userLoggedIn; ?>">
+					<img src="<?php	echo $user['profile_pic']; ?>" alt="Profile Photo" class="img-thumbnail">
+				</a>
+
+				<div class="user_details_left">
+					<a href="<?php echo $userLoggedIn; ?>">
+						<?php  
+							echo $user['first_name'] . " " . $user['last_name'];
+							echo "<br>";
+						?>
+					</a>
+
+					<?php  
+						echo "Posts: " . $user['num_posts'];
+						echo "<br>";
+						echo "Likes: " . $user['num_likes'];
+					?>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="col-sm-8">
+			<div class="main_column">
+				<form action="index.php" method="POST" role="form" class="post_form">
+
+					<div class="form-group">
+						<textarea name="post_text" id="post_text" rows="4" placeholder="Got something to share?" class="form-control"></textarea>
+					</div>
+					
+
+
+					<button type="submit" class="btn btn-primary" name="post" id="post_button">Post</button>
+				</form>
+
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+</div>
 	
 </body>
 </html>
